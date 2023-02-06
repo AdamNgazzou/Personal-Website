4116dc9199129d44c81d799acc48fa5de999d201
AdamNgazzou <117188411+AdamNgazzou@users.noreply.github.com>
2023-02-06 23:25:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -59,4 +59,16 @@ function slide(slider){
 	yeb(slider);
 }
 */
+window.addEventListener("DOMContentLoaded", function(e) {
 
+    // Original JavaScript code by Chirp Internet: chirpinternet.eu
+    // Please acknowledge use of this code by including this header.
+
+    var stage = document.getElementById("image");
+    var fadeComplete = function(e) { stage.appendChild(arr[0]); };
+    var arr = stage.getElementsByTagName("a");
+    for(var i=0; i < arr.length; i++) {
+      arr[i].addEventListener("animationend", fadeComplete, false);
+    }
+
+  }, false);
